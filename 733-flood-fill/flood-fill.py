f22ee58dc76db9d8e1add7512f197e9655fb1c86
Krishna Shah <76779487+krishnasah371@users.noreply.github.com>
2025-12-18 22:22:37 -0600

    Time: 0 ms (100.00%) | Memory: 17.8 MB (88.93%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -3,33 +3,35 @@ class Solution:
 
         if not image:
             return 0
-         
+
         rows, cols = len(image), len(image[0])
-        visit = set()
-        orc = image[sr][sc]
+
+        visited = set()
+        
 
         def bfs(r, c):
             q = collections.deque()
-            visit.add((r, c))
             q.append((r, c))
+            visited.add((r, c))
 
             while q:
-                row, col = q.popleft()
-                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                rol, col = q.popleft()
+
+                directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
                 for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if r in range(rows) and c in range(cols) and image[r][c] == orc and (r,c) not in visit:
+                    r, c = rol + dr, col + dc
+
+                    if r in range(rows) and c in range(cols) and image[sr][sc] == image[r][c] and (r, c) not in visited:
                         image[r][c] = color
-                        q.append((r,c))
-                        visit.add((r,c))
+                        q.append((r, c))
+                        visited.add((r, c))
+            
 
-
-        # for r in range(rows):
-        #     for c in range(cols):
-        #         if image[r][c] == image[sr][sc]:
-        #             image[r][c] = color
-        #             bfs(r, c)
         bfs(sr, sc)
         image[sr][sc] = color
         return image
+
+    
+
+        
